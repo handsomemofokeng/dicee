c78dd92a26b6d45db97f9c0db33894db0ed14825
Handsome Mofokeng <handsomem23@hotmail.com>
2021-01-18 23:29:24 +0200

    added an event for TextButton (was FlatButton)

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,19 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
               child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('b1 pressed');
+              },
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: TextButton(
+              child: Image.asset('images/dice4.png'),
+              onPressed: () {
+                print('b2 pressed');
+              },
             ),
           ),
         ],
